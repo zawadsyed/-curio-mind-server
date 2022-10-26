2cd27c45a28390b453c755c42549e63a0a102888
Syed Zawad <syedzawadd@gmail.com>
2022-10-26 21:51:39 +0600

    first get api created

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const app = express();
 const port = process.env.port || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Express Server is running');
+})
+app.listen(port, () => {
+    console.log(`app server is running on ${port}`);
+})
